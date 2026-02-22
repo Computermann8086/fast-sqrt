08abbad1efde367bdfa68b873479c81c795a042f
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2026-02-22 20:42:12 +0100

    Create fast_sqrt.h

---
+++ fast_sqrt.c
@@ -0,0 +1,23 @@
+#include "fast_sqrt.h"
+
+static inline float fast_sqrt(float n){
+  if(n <= 0){
+    return *(float*)&(0x7FC00000U); // return NaN
+  }
+
+  float est = n;
+  long nf;
+
+  nf = *(long*)&est;
+  nf = (((nf >> 1) << 23) - 0x3F7A7EFA)^0x80000000U;
+  est = *(float*)&;
+
+  float est_prev = est+2*PRECISION;
+  int iter = 0;
+  while(*(float*)&((*(long*)&(est_prev - est))&~(1UL << 31)) > PRECISION && iter++ < ITER_MAX){  // the same as fabs(est_prev - est) but without the function call
+    est_prev = est;
+    est = 0.5F*(est + (n/est));
+  }
+  
+  return est;
+}
