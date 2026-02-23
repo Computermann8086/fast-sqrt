6fc915e74a65a5679fbfdb6c15516e8c01aaff58
Computermann8086 <salchenrichsen@salangenskole.no>
2026-02-23 16:41:42 +0100

    Update: Made it so people can chose which version they want to use

--- fast_sqrt.c
+++ fast_sqrt.c
@@ -12,18 +12,27 @@ static inline float fast_sqrt(float n){
 
   nf = *(long*)&est;
   /*
-  * Original Bit Hack
+  * Original Bit Hack for sqrt(x)
   * nf = (((nf >> 1) << 23) - 0x3F7A7EFA)^0x80000000U;
   */
-  nf = ((nf&~0x7F800000U | ((((int)((nf & 0x7F800000U) >> 23)-127)>>1) & 0xff)<<23)-0x3F7A7EFA)^0x80000000U; // I swear, this is just black magic at this point
+
+  /* 
+  * Bit hack for sqrt(x)
+  * nf = ((nf&~0x7F800000U | ((((int)((nf & 0x7F800000U) >> 23)-127)>>1) & 0xff)<<23)-0x3F7A7EFA)^0x80000000U; // I swear, this is just black magic at this point
+  */
+   
+  /* Original Quake III 1/sqrt(x) estimate*/
+  nf = 0x5f3759df-(nf >> 1);
+
   est = *(float*)&nf;
 
   float est_prev = est+2*PRECISION;
   int iter = 0;
   while(*(float*)&(long){(*(long*)&(float){est_prev-est})&~(1UL << 31)} > PRECISION && iter++ < ITER_MAX){  // the same as fabs(est_prev - est) but without the function call
     est_prev = est;
-    //est = 0.5F*(est + (n/est)); // Normal Newton
-    est *= (onenhalf - n2*est*est);
+    //est = 0.5F*(est + (n/est)); // Normal Newton for sqrt(x)
+    est *= (onenhalf - n2*est*est); // Newton for 1/sqrt(x)
   }
-  return est*n;
+  // return est;  // For sqrt(x) newton
+  return est*n;  // For 1/sqrt(x) newton
 }
